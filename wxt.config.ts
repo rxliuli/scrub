2d908fedf82b2783d39d0a708626901330ffb94c
rxliuli <rxliuli@gmail.com>
2026-06-01 18:39:34 +0800

    init project

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   modules: ['@wxt-dev/module-react', 'wxt-module-safari-xcode'],
   safariXcode: {
     appCategory: 'public.app-category.productivity',
-    bundleIdentifier: 'com.rxliuli.browser-extension-template',
+    bundleIdentifier: 'com.rxliuli.site-eraser',
     developmentTeam: 'N2X78TUUFG',
   },
   vite: () => ({
@@ -17,7 +17,7 @@ export default defineConfig({
   manifestVersion: 3,
   manifest: (env) => {
     const manifest: UserManifest = {
-      name: 'Browser Extension Template',
+      name: 'Site Eraser',
       description:
         'A template for WXT, a WebExtension framework based on Vite and React',
       permissions: ['storage'],
@@ -34,7 +34,7 @@ export default defineConfig({
           '128': 'icon/128.png',
         },
       },
-      homepage_url: 'https://rxliuli.com/project/browser-extension-template',
+      homepage_url: 'https://rxliuli.com/project/site-eraser',
     }
     if (env.browser === 'firefox') {
       manifest.browser_specific_settings = {
